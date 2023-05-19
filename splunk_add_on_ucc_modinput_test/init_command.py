e22a13eee91c22cbf90e353aba3271bf29e292d3
Hubert Sękowski <hsekowski@splunk.com>
2023-05-19 16:02:22 +0000

    fix

--- splunk_add_on_ucc_modinput_test/init_command.py
+++ splunk_add_on_ucc_modinput_test/init_command.py
@@ -17,6 +17,10 @@ import logging
 from pathlib import Path
 import shutil
 from python_on_whales import docker
+# from importlib import resources as resources_lib
+# from splunk_add_on_ucc_modinput_test import resources as resources_dir
+from importlib_resources import files
+from splunk_add_on_ucc_modinput_test import resources
 
 # from cookiecutter import exceptions, main
 
@@ -40,7 +44,7 @@ def init(
 # Download the rest.mustache file (wget https://raw.githubusercontent.com/swagger-api/swagger-codegen/master/modules/swagger-codegen/src/main/resources/python/rest.mustache)
 # Splunk does not expect body for DELETE requests, so we need to revert modifications done for https://github.com/swagger-api/swagger-codegen/issues/9558 (sed "s/request_body[[:blank:]]=[[:blank:]]\'{}\'/request_body = None/g" rest.mustache > generator/rest.mustache). If you want to understand exactly which line of rest.mustache is affected: https://github.com/swagger-api/swagger-codegen/blob/master/modules/swagger-codegen/src/main/resources/python/rest.mustache#L150
     # cp -R ${PWD}/swagger-codegen-generators/ ${TMP}
-    shutil.copy(str(Path() / "splunk_add_on_ucc_modinput_test/swagger-codegen-generators/src/main/resources/handlebars/python/rest.mustache"), str(generator_path))
+    shutil.copy(str(files(resources).joinpath('swagger-codegen-generators/src/main/resources/handlebars/python/rest.mustache')), str(generator_path))
 # Create client (docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli-v3 generate -i /local/openapi.json -l python -o /local/restapi_client -t /local/generator/); it should appear in restapi_client directory
     docker.run(
         "swaggerapi/swagger-codegen-cli-v3",
