b9a5c8a0a4bec68340c269cfc75bce3fb57f13a0
Hubert Sękowski <hsekowski@splunk.com>
2023-07-07 19:07:30 +0200

    fix: new line in base64

--- splunk_add_on_ucc_modinput_test/common/utils.py
+++ splunk_add_on_ucc_modinput_test/common/utils.py
@@ -55,10 +55,19 @@ def get_from_environment_variable(
 
 
 class Base64:
+
+    @staticmethod
+    def _remove_ending_chars(string: str) -> str:
+        if len(string) == 0: return string
+        CHARS_TO_BE_REMOVED = ['\n']
+        for i in range(len(string),-1,-1):
+            if string[i-1] not in CHARS_TO_BE_REMOVED:
+                break
+        return string[:i]
     
     @staticmethod
     def encode(string: str) -> str:
-        _bytes = string.encode('utf-8')
+        _bytes = Base64._remove_ending_chars(string=string).encode('utf-8')
         base64_encoded = base64.b64encode(_bytes)
         base64_string = base64_encoded.decode('utf-8')
         return base64_string
@@ -69,8 +78,8 @@ class Base64:
     ) -> str:
         base64_bytes = base64_string.encode('utf-8')
         decoded_bytes = base64.b64decode(base64_bytes)
-        string = decoded_bytes.decode('utf-8').replace('\n', '')
-        return string
+        string = decoded_bytes.decode('utf-8')
+        return Base64._remove_ending_chars(string=string)
 
 
 def get_epoch_timestamp() -> float:
--- tests/common/test_utils.py
+++ tests/common/test_utils.py
@@ -22,22 +22,73 @@ def test_get_from_environment_variable_string_function(monkeypatch):
 def test_get_from_environment_variable_optional(monkeypatch):
     assert utils.get_from_environment_variable("DOES_NOT_EXIST", is_optional=True) == None
 
-# def test_get_from_environment_variable_alternative_environment_variable(monkeypatch):
-#     k = 'DOES_NOT_EXIST'
-#     k_alternative = "EXISTING_ENV_VARIABLE"
-#     v_alternative = "some_value"
-#     monkeypatch.setenv(k_alternative, v_alternative)
-#     assert utils.get_from_environment_variable(k,alternative_environment_variable=k_alternative) == v_alternative
-    
-#     k = 'DOES_EXIST'
-#     v = 'value'
-#     monkeypatch.setenv(k, v)
-#     assert utils.get_from_environment_variable(k,alternative_environment_variable=k_alternative) == v
+def test_base64():
 
-# def test_get_from_environment_variable_alternative_with_function(monkeypatch):
-#     k = 'DOES_NOT_EXIST'
-#     k_alternative = "EXISTING_ENV_VARIABLE"
-#     some_str = 'I am complex enough to be encoded'
-#     v_alternative = utils.Base64.encode(some_str)
-#     monkeypatch.setenv(k_alternative, v_alternative)
-#     assert utils.get_from_environment_variable(k,alternative_environment_variable=k_alternative,string_function=utils.Base64.decode) == some_str
+    def _encode_decode_assert(input_string: str, expected: str=None):
+        if expected == None: expected = input_string
+        encoded_s = utils.Base64.encode(string=input_string)
+        actual = utils.Base64.decode(base64_string=encoded_s)
+        assert actual == expected
+    
+    _encode_decode_assert('I w@nt t0 encode th^s!')
+    input_string = '''Th!s
+is
+multiline'''
+    _encode_decode_assert(input_string=input_string, expected=input_string)
+    expected = '''Th!s
+is
+multiline'''
+    input_string = '''Th!s
+is
+multiline
+
+
+
+'''
+    _encode_decode_assert(input_string=input_string, expected=expected)
+
+
+
+
+    input_string = '''Th!s
+
+is
+
+multiline'''
+    _encode_decode_assert(input_string=input_string, expected=input_string)
+    expected = '''Th!s
+
+is
+
+multiline'''
+    input_string = '''Th!s
+
+is
+
+multiline
+
+
+
+'''
+    _encode_decode_assert(input_string=input_string, expected=expected)
+
+def test_base64_remove_ending_chars():
+
+    expected = ''
+
+    input = expected
+    actual = utils.Base64._remove_ending_chars(string=input)
+    assert actual == expected
+
+    input = '''
+'''
+    actual = utils.Base64._remove_ending_chars(string=input)
+    assert actual == expected
+
+    input = '''
+
+
+
+'''
+    actual = utils.Base64._remove_ending_chars(string=input)
+    assert actual == expected
