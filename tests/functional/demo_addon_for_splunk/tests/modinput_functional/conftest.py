7c2ceb757b4f29fac563bdd77a5fa64da2435d40
Hubert Sękowski <hsekowski@splunk.com>
2023-11-25 07:56:08 +0100

    chore: initial for xdist

--- tests/functional/demo_addon_for_splunk/tests/modinput_functional/conftest.py
+++ tests/functional/demo_addon_for_splunk/tests/modinput_functional/conftest.py
@@ -1,4 +1,5 @@
 #   DO NOT MODIFY CODE IN THIS FILE
+from typing import Generator
 import pytest
 from splunk_add_on_ucc_modinput_test.common.splunk_instance import (
     Configuration as SplunkConfiguration,
@@ -10,13 +11,27 @@ from tests.modinput_functional.ta import Configuration as TaConfiguration
 
 
 #   DO NOT MODIFY CODE IN THIS FILE
-@pytest.fixture(scope="session")
-def configuration():
+def create_ta_configuration_and_set_up() -> TaConfiguration:
     ta_configuration = TaConfiguration(
         splunk_configuration=SplunkConfiguration(),
         vendor_product_configuration=VendorProductConfiguration(),
     )
     ta_configuration.set_up(ta_configuration.api_instance)
+    return ta_configuration
+
+
+@pytest.fixture(scope="session")
+def configuration(
+    tmp_path_factory: pytest.TempPathFactory, worker_id: str
+) -> Generator[TaConfiguration, None, None]:
+    # ta_configuration = TaConfiguration(
+    #     splunk_configuration=SplunkConfiguration(),
+    #     vendor_product_configuration=VendorProductConfiguration(),
+    # )
+    # ta_configuration.set_up(ta_configuration.api_instance)
+    if not worker_id:
+        ta_configuration = create_ta_configuration_and_set_up()
+
     yield ta_configuration
     ta_configuration.tear_down(ta_configuration.api_instance)
 
