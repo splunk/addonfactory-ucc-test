72b1d8d8de4d23d26e33ed85178115e9e44b5611
Hubert Sękowski <hsekowski@splunk.com>
2023-11-25 12:16:36 +0100

    chore: let us see

--- tests/functional/demo_addon_for_splunk/tests/modinput_functional/conftest.py
+++ tests/functional/demo_addon_for_splunk/tests/modinput_functional/conftest.py
@@ -1,6 +1,9 @@
 #   DO NOT MODIFY CODE IN THIS FILE
 from typing import Generator
+from filelock import FileLock
+import json
 import pytest
+from splunk_add_on_ucc_modinput_test.common import utils
 from splunk_add_on_ucc_modinput_test.common.splunk_instance import (
     Configuration as SplunkConfiguration,
 )
@@ -30,7 +33,35 @@ def configuration(
     # )
     # ta_configuration.set_up(ta_configuration.api_instance)
     if not worker_id:
-        ta_configuration = create_ta_configuration_and_set_up()
+        ta_configuration = TaConfiguration(
+            splunk_configuration=SplunkConfiguration(),
+            vendor_product_configuration=VendorProductConfiguration(),
+        )
+        ta_configuration.set_up(ta_configuration.api_instance)
+    else:
+        # get the temp directory shared by all workers
+        root_tmp_dir = tmp_path_factory.getbasetemp().parent
+
+        fn = root_tmp_dir / "data.json"
+        with FileLock(str(fn) + ".lock"):
+            if fn.is_file():
+                data = json.loads(fn.read_text())
+                utils.Common().start_timestamp = float(data)
+                ta_configuration = TaConfiguration(
+                    splunk_configuration=SplunkConfiguration(),
+                    vendor_product_configuration=VendorProductConfiguration(),
+                )
+        # ta_configuration.set_up(ta_configuration.api_instance)
+            else:
+                data = utils.Common().start_timestamp
+                fn.write_text(json.dumps(data))
+                ta_configuration = TaConfiguration(
+                    splunk_configuration=SplunkConfiguration(),
+                    vendor_product_configuration=VendorProductConfiguration(),
+                )
+                ta_configuration.set_up(ta_configuration.api_instance)
+        # return data
+
 
     yield ta_configuration
     ta_configuration.tear_down(ta_configuration.api_instance)
