bea16f9cf7a53a11477643337f42c18073dc7e6e
Hubert Sękowski <hsekowski@splunk.com>
2025-03-10 04:30:02 +0100

    chore:

--- splunk_add_on_ucc_modinput_test/common/splunk_instance.py
+++ splunk_add_on_ucc_modinput_test/common/splunk_instance.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 # mypy: disable-error-code="attr-defined,arg-type"
 
 import time
+from typing import Optional
 import pytest
 import requests  # type: ignore
 from requests.adapters import HTTPAdapter, Retry  # type: ignore
@@ -82,9 +83,9 @@ class Configuration:
         client_service: SplunkServicePool,
         *,
         is_cloud: bool = False,
-        acs_stack: str = None,
-        acs_server: str = None,
-        splunk_token: str = None,
+        acs_stack: str | None = None,
+        acs_server: str | None = None,
+        splunk_token: str | None = None,
     ) -> Index:
         if Configuration.get_index(index_name, client_service):
             reason = f"Index {index_name} already exists"
@@ -112,21 +113,21 @@ class Configuration:
     __instances: dict[tuple[str, str, str], Configuration] = {}
 
     @classmethod
-    def collect_host(cls) -> str:
+    def collect_host(cls) -> str | None:
         return utils.get_from_environment_variable("MODINPUT_TEST_SPLUNK_HOST")
 
     @classmethod
-    def collect_port(cls) -> str:
+    def collect_port(cls) -> str | None:
         return utils.get_from_environment_variable("MODINPUT_TEST_SPLUNK_PORT")
 
     @classmethod
-    def collect_username(cls) -> str:
+    def collect_username(cls) -> str | None:
         return utils.get_from_environment_variable(
             "MODINPUT_TEST_SPLUNK_USERNAME"
         )
 
     @classmethod
-    def collect_password(cls) -> str:
+    def collect_password(cls) -> str | None:
         return utils.get_from_environment_variable(
             "MODINPUT_TEST_SPLUNK_PASSWORD_BASE64",
             string_function=utils.Base64.decode,
@@ -147,7 +148,7 @@ class Configuration:
         )
 
     @classmethod
-    def collect_splunk_token(cls, is_optional) -> str | None:
+    def collect_splunk_token(cls, is_optional: bool) -> str | None:
         return utils.get_from_environment_variable(
             "MODINPUT_TEST_SPLUNK_TOKEN_BASE64",
             string_function=utils.Base64.decode,
@@ -155,14 +156,14 @@ class Configuration:
         )
 
     @classmethod
-    def collect_acs_server(cls, is_optional) -> str | None:
+    def collect_acs_server(cls, is_optional: bool) -> str | None:
         return utils.get_from_environment_variable(
             "MODINPUT_TEST_ACS_SERVER",
             is_optional=is_optional,
         )
 
     @classmethod
-    def collect_acs_stack(cls, is_optional) -> str | None:
+    def collect_acs_stack(cls, is_optional: bool) -> str | None:
         return utils.get_from_environment_variable(
             "MODINPUT_TEST_ACS_STACK",
             is_optional=is_optional,
