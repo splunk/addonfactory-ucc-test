b54a582d57cd05918029e349feaa5202dcbfa63c
Marcin Kolasinski <mkolasinski@splunk.com>
2026-08-27 11:49:16 +0200

    fix: preserve selected parameterized tests on v2.0.1

---
+++ tests/functional/flow/scenarios/partially_selected_parametrized.py
@@ -0,0 +1,31 @@
+import logging
+from typing import Dict
+
+import pytest
+
+from splunk_add_on_ucc_modinput_test.functional.decorators import (
+    bootstrap,
+    forge,
+)
+
+logger = logging.getLogger("ucc-modinput-test")
+
+
+def provide_selected_value(parametrized_value: str) -> Dict[str, str]:
+    logger.info(f"provide_selected_value value={parametrized_value}")
+    return {"forged_value": parametrized_value}
+
+
+@pytest.mark.parametrize(
+    "parametrized_value",
+    [
+        pytest.param("selected", marks=pytest.mark.selected),
+        pytest.param("deselected"),
+    ],
+)
+@bootstrap(forge(provide_selected_value, scope="function"))
+def test_partially_selected_parametrized(
+    parametrized_value: str,
+    forged_value: str,
+) -> None:
+    assert forged_value == parametrized_value
